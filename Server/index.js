4d2eaf04d7ec749f35069dab305840e76d59e0a2
BlackPing <bug_ping@naver.com>
2021-06-09 15:18:29 +0900

    config check

--- Server/index.js
+++ Server/index.js
@@ -78,6 +78,8 @@ API_init('./Server/API', '');
 console.log('root path: ' + path.join(__dirname, '../dist'))
 app.use('/', express.static(path.join(__dirname, '../dist')));
 
+console.log(config.Server);
+
 config.Server.SSL ? sslServer() :
 app.listen(config.Server.port, config.Server.ip, () => {
 	console.log('ip: ', config.Server.ip, 'port', config.Server.port)
